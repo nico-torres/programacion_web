bcabcb28fabba8b977a3ea395057cd2a4997bb22
lucks331988 <villalbalucas25@gmail.com>
2021-08-12 16:05:36 -0300

    23. Crear un formulario

--- proyecto/ProyectoInfo/ProyectoInfo/settings.py
+++ proyecto/ProyectoInfo/ProyectoInfo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'juego.apps.JuegoConfig'
+    'juego.apps.UsuarioConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'ProyectoInfo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- proyecto/ProyectoInfo/ProyectoInfo/urls.py
+++ proyecto/ProyectoInfo/ProyectoInfo/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from usuario import views as usuario_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('juego', include('juego.urls')),
+    path('registro', usuario_views.registro),
+    path('login', usuario_views.login),
+    path('logout', usuario_views.logout),
+
 ]
